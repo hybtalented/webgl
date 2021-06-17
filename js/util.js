48345be5369d0a55940b0c99a3710feac43e3922
Youbiao He <hybtalented@163.com>
2021-06-17 21:31:25 +0800

    阴影 - 添加帧缓冲区初始化部分

--- js/util.js
+++ js/util.js
@@ -47,6 +47,36 @@ class Cube {
   }
 }
 
+class Sphere {
+  /**
+   * @summary 从包围盒获取到包围球
+   * @param {Cube} cube
+   * @returns
+   */
+  static fromCute(cube) {
+    return new Sphere(cube.center, Math.max(...cube.size.elements) / 2);
+  }
+  /**
+   * @summary 构造一个包围球
+   * @param {Vector3} center 包围球的中心
+   * @param {number} radius 包围球的半径
+   */
+  constructor(center, radius) {
+    this.center = center;
+    this.radius = radius;
+  }
+
+  getViewMatrix(fovy, { h_ratio }) {
+    const angle = ((fovy / 2) * Math.PI) / 180.0;
+    const eyeHeight = (this.radius * 2 * h_ratio) / 2 / angle;
+    return new Matrix4().lookAt(0, 0, eyeHeight, 0, 0, 0, 0, 1, 0);
+  }
+  get modelMatrix() {
+    const center = this.center.elements;
+    return new Matrix4().translate(-center[0], -center[1], -center[2]);
+  }
+}
+
 function cubeGenerator(center, size) {
   var min_x = center[0] - size[0] / 2,
     max_x = center[0] + size[0] / 2;
@@ -390,46 +420,37 @@ class DEMLoader {
    * @param {string} texture_image 纹理图片
    * @param {{format:number, id: number, location: WebGLUniformLocation}} options
    */
-  async loadTextrue(gl, texture_image, options) {
+  loadTexture(gl, texture_image, options) {
     const merged_option = Object.assign({ id: 0, format: gl.RGB }, options);
     const { id } = merged_option;
-    if (!options.location) {
-      console.warn("sampler location must be defined in options");
-      return [];
-    }
     return new Promise((resolve) => {
       var image = new Image();
       image.onload = () => {
-        var texture = gl.createTexture();
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
-        gl.activeTexture(gl[`TEXTURE${id}`]);
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        const texture = new WebGLTextureWrapper(gl, merged_option.format);
 
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texImage2D(
-          gl.TEXTURE_2D,
-          0,
-          merged_option.format,
-          merged_option.format,
-          gl.UNSIGNED_BYTE,
-          image
-        );
-        gl.uniform1i(merged_option.location, id);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+        texture.bind(id);
+        texture.setParams();
+        texture.bindImage(image);
+
         const max = this.cube.max.elements;
         const min = this.cube.min.elements;
+        const size = [max[0] - min[0], max[1] - min[1]];
         resolve(
-          this.data.map((vertex) =>
-            vertex
-              .slice(0, 3)
-              .concat([
-                (vertex[0] - min[0]) / (max[0] - min[0]),
-                (vertex[1] - min[1]) / (max[1] - min[1]),
-              ])
-          )
+          this.data.map((vertex) => {
+            let data = vertex.slice(0, 3);
+            if (merged_option.with_normal) {
+              data = data.concat(vertex.slice(6, 9));
+            }
+            return data.concat([
+              (vertex[0] - min[0]) / size[0],
+              (vertex[1] - min[1]) / size[1],
+            ]);
+          })
         );
+        if (merged_option.location) {
+          gl.uniform1i(merged_option.location, id);
+        }
       };
       image.src = texture_image;
     });
@@ -493,5 +514,343 @@ class FileUtils {
       };
       reader.readAsText(file);
     });
+  }
+}
+/**
+ * @summary WebGL program 对象封装
+ * @typedef {{name:string;type:string;location?: WebGLUniformLocation|number}} VarMetaInfo
+ */
+class WebGLProgramWrapper {
+  static attr_rec_reg = /attribute\s+([\w\d_]+)\s+([\w\d_]+)\s*;/g;
+  static uniform_rec_reg = /uniform\s+([\w\d_]+)\s+([\w\d_]+)\s*;/g;
+  /**
+   * @summary
+   * @param {WebGLRenderingContext} gl WebGL 渲染上下文对象
+   */
+  constructor(gl) {
+    this.gl = gl;
+    this.meta_info = {};
+    this.program = gl.createProgram();
+  }
+  /**
+   * @summary 创建着色器
+   * @param {number} type 着色器类型
+   * @param {string} source 着色器源代码
+   */
+  createShader(type, source) {
+    const { gl, program } = this;
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    gl.attachShader(program, shader);
+    return shader;
+  }
+
+  /**
+   * @summary 初始化WebGL程序
+   * @param {string} vShaderSource
+   * @param {string} fShaderSource
+   */
+  init(vShaderSource, fShaderSource) {
+    const { gl, program } = this;
+    this.shader = this.createShader(gl.VERTEX_SHADER, vShaderSource);
+    this.fragment = this.createShader(gl.FRAGMENT_SHADER, fShaderSource);
+    gl.linkProgram(program);
+    this.initVariables(vShaderSource, fShaderSource);
+  }
+  /**
+   * @summary 初始化程序内部变了
+   * @param {string} vShaderSource 顶点着色器源代码
+   * @param {string} fShaderSource 片元着色器源代码
+   */
+  initVariables(vShaderSource, fShaderSource) {
+    WebGLProgramWrapper.getAttributes(vShaderSource).forEach((attr) => {
+      this.addMetaData("attribute", attr);
+    });
+    WebGLProgramWrapper.getUniforms(vShaderSource).forEach((uniform) => {
+      this.addMetaData("uniform", uniform);
+    });
+    WebGLProgramWrapper.getUniforms(fShaderSource).forEach((uniform) => {
+      this.addMetaData("uniform", uniform);
+    });
+  }
+  /**
+   * @summary 初始化 Program 属性
+   * @param {string} source 属性列
+   * @returns {VarMetaInfo[]}
+   */
+  static getAttributes(source) {
+    const attribute_info = [];
+    for (const attr of source.matchAll(WebGLProgramWrapper.attr_rec_reg)) {
+      if (attr.length === 3) {
+        attribute_info.push({ name: attr[2], type: attr[1] });
+      } else {
+        console.error(`invalid attrbute ${attr} for source`);
+      }
+    }
+    return attribute_info;
+  }
+
+  /**
+   * @summary 初始化 uniforms
+   * @param {string} source uniform 列表
+   * @returns {VarMetaInfo[]} 初始化失败的 uniform 列表
+   */
+  static getUniforms(source) {
+    const uniform_info = [];
+    for (const uniform of source.matchAll(
+      WebGLProgramWrapper.uniform_rec_reg
+    )) {
+      if (uniform.length === 3) {
+        uniform_info.push({ name: uniform[2], type: uniform[1] });
+      } else {
+        console.error(`invalid uniform ${uniform} for source`);
+      }
+    }
+    return uniform_info;
+  }
+  /**
+   *
+   * @param {string}} type
+   * @param {VarMetaInfo} info
+   */
+  addMetaData(type, info) {
+    const { meta_info, gl, program } = this;
+
+    if (meta_info.hasOwnProperty(info.name)) {
+      const origin_info = meta_info[info.name];
+      if (origin_info.meta_type !== type) {
+        console.error(
+          `${type} ${info.name} declaretion with differet meta type ${
+            meta_info[info.name].meta_type
+          }`
+        );
+      } else if (type === "attribute") {
+        console.error("redeclaretion of attibute", info.name);
+        return;
+      } else if (type === "uniform" && info.type !== origin_info.type) {
+        console.error(
+          `${type} ${info.name} declaretion with differet variable type ${info.type}`
+        );
+      }
+      return;
+    }
+    switch (type) {
+      case "uniform":
+        info.location = gl.getUniformLocation(program, info.name);
+        if (!info.location) {
+          console.warn(`location of uniform ${info.name} is not found`);
+          return;
+        }
+        this[info.name] = info.location;
+        break;
+      case "attribute":
+        info.location = gl.getAttribLocation(program, info.name);
+        if (info.location < 0) {
+          console.warn(`location of attribute ${info.name} is not found`);
+          return;
+        }
+        this[info.name] = info.location;
+        break;
+      default:
+        break;
+    }
+    meta_info[info.name] = {
+      ...info,
+      meta_type: type,
+    };
+  }
+  use() {
+    const { gl, program } = this;
+    gl.useProgram(program);
+  }
+  unuse() {
+    this.gl.useProgram(null);
+  }
+}
+class WebGLFrameBufferWrapper {
+  /**
+   *
+   * @param {WebGLRenderingContext} gl
+   * @param {number} width 离屏渲染的宽度
+   * @param {number} height 离屏渲染的高度
+   */
+  constructor(gl, width, height) {
+    this.gl = gl;
+    this.width = width;
+    this.height = height;
+    this.framebuffer = gl.createFramebuffer();
+  }
+  /**
+   * @summary 将framebufer 绑定的正缓冲区对象
+   */
+  bind() {
+    const { gl, framebuffer, width, height } = this;
+    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+    gl.viewport(0, 0, width, height);
+  }
+
+  unbind() {
+    const { gl } = this;
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  }
+
+  /**
+   *
+   * @param {} attachment 指定渲染缓冲区关联的帧缓冲区对象类型
+   */
+  createRenderBuffer(attachment = gl.DEPTH_ATTACHMENT) {
+    const { gl, width, height } = this;
+    const renderBuffer = gl.createRenderBuffer();
+    if (!renderBuffer) {
+      console.error("failed to create renderbuffer object");
+    }
+    gl.bindRenderbuffer(gl.RENDERBUFFER, renderBuffer);
+    gl.renderbufferStorage(
+      gl.RENDERBUFFER,
+      gl.DEPTH_COMPONENT16,
+      width,
+      height
+    );
+    // 将
+    gl.framebufferRenderbuffer(
+      gl.FRAMEBUFFER,
+      attachment,
+      gl.RENDERBUFFER,
+      renderBuffer
+    );
+
+    return renderBuffer;
+  }
+  /**
+   * @summary 获取当前帧缓冲区状态
+   */
+  status() {
+    const { gl } = this;
+    const status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+    if (gl.FRAMEBUFFER_COMPLETE !== status) {
+      console.log(`frame buffer object is incomplete: ${status.toString()}`);
+    }
+    return status;
+  }
+}
+class WebGLBufferWrapper {
+  /**
+   * 封装 WebGL 的 buffer 对象
+   * @param {WebGLRenderingContext} gl
+   * @param {number} target 缓冲区关联的对象
+   */
+  constructor(gl, target) {
+    this.gl = gl;
+    this.target = target;
+    this.buffer = gl.createBuffer();
+  }
+  /**
+   * @summary 初始化buffer 对象数据
+   * @param {number} usage
+   * @param {Uint8Array|Uint16Array|Uint32Array|Float32Array|Float64Array} data
+   * @param {number} stride 每一节点的元素个数
+   */
+  init(usage, data, stride = 1) {
+    const { gl, target } = this;
+    gl.bufferData(target, data, usage);
+    this.ElementSize = data.BYTES_PER_ELEMENT;
+    this.stride = stride * this.ElementSize;
+    this.size = Math.floor(data.length / stride);
+  }
+  /**
+   * @summary 将当前buffer对象绑定到gl的对应buffer上
+   * @param {number} target
+   */
+  bind() {
+    const { gl, buffer, target } = this;
+    gl.bindBuffer(target, buffer);
+  }
+  unbind() {
+    const { gl, target } = this;
+    gl.bindBuffer(target, null);
+  }
+  /**
+   * @summary 将buffer 对象绑定到属性上
+   * @param {number} attribute 属性
+   * @param {number} size 元素数量
+   * @param {number} type 元素类型
+   * @param {boolean} normalized 是否进行规范化
+   * @param {number} offset 偏移量
+   */
+  bindAttibute(attribute, size, type, offset = 0, normalized = false) {
+    const { gl, ElementSize, stride } = this;
+    gl.vertexAttribPointer(
+      attribute,
+      size,
+      type,
+      normalized,
+      stride,
+      ElementSize * offset
+    );
+  }
+}
+
+class WebGLTextureWrapper {
+  /**
+   * 封装 WebGL 的 texture 对象
+   * @param {WebGLRenderingContext} gl
+   * @param {format} 纹理格式
+   */
+  constructor(gl, format = gl.RGBA) {
+    this.gl = gl;
+    this.format = format;
+    this.texture = gl.createTexture();
+    if (!this.texture) {
+      console.log(`createFrameBuffer Texture object faild`);
+      return;
+    }
+  }
+
+  /**
+   * @summary 将纹理对象绑定到对应的纹理上
+   */
+  bind(id) {
+    const { gl, texture } = this;
+    gl.activeTexture(gl[`texture${id}`]);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+  }
+
+  setParams() {
+    const { gl } = this;
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  }
+  /**
+   * 将纹理对象绑定的帧缓冲区的对象上
+   * @param {WebGLFrameBufferWrapper} framebuffer
+   * @param {number} attachment 关联的帧缓冲区对象的类型
+   */
+  bindFramebuffer(framebuffer, attachment = this.gl.COLOR_ATTACHMENT0) {
+    const { gl, texture, format } = this;
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      format,
+      framebuffer.width,
+      framebuffer.height,
+      0,
+      format,
+      gl.UNSIGNED_BYTE,
+      null
+    );
+    gl.framebufferTexture2D(
+      gl.FRAMEBUFFER,
+      attachment,
+      gl.TEXTURE_2D,
+      texture,
+      0
+    );
+  }
+  bindImage(image) {
+    const { gl, format } = this;
+    gl.texImage2D(gl.TEXTURE_2D, 0, format, format, gl.UNSIGNED_BYTE, image);
   }
 }
